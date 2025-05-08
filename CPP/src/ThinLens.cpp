ef5decf71d24688bd72750984a778779f5604b04
Grabowski02 <vitus.szabi@gmail.com>
2025-05-08 11:31:40 +0300

    Writing the Thin Lens Calculate functions

--- CPP/src/ThinLens.cpp
+++ CPP/src/ThinLens.cpp
@@ -1,7 +1,13 @@
 #include "ThinLens.h"
+#include <math.h>
 
 ThinLens::ThinLens(double x, double y, double f):Lens(x, y, f){}
-Image ThinLens::Calculate(ImagingSubject){
-    // TODO writing this method
-    return Image(0, 0, 0);
+Image ThinLens::Calculate(ImagingSubject is){
+    double x_is = is.getX();
+    double y_is = is.getY();
+
+    double x_im = pow(1/getF() - 1/x_is, -1);
+    double y_im = -x_im/x_is*y_is;
+
+    return Image(x_im, y_im, x_im > 0);
 }
