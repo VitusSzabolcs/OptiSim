5168ef67cfdcef3b610d56f15c36e5bfe8ea245e
Vitus Szabolcs <vitus.szabi@gmail.com>
2025-05-31 21:22:58 +0300

    gil

--- Python/bind/optisim_bindings.cpp
+++ Python/bind/optisim_bindings.cpp
@@ -8,6 +8,7 @@ void bind_optical_objects(py::module_& m);
 void bind_optical_system(py::module_& m);
 
 PYBIND11_MODULE(optisim, m) {
+    py::gil_scoped_acquire gil;
     m.doc() = "OptiSim: A C++ Optical Simulation Library bound to Python using pybind11.";
 
     bind_optical_objects(m);
