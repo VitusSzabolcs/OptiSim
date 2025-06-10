dea46f7bcea5aebcd4102476ed9b2c4f5dd5f6d8
Bacs Tamas <bacstamas01@gmail.com>
2025-06-10 20:28:29 +0300

    C++ testing

--- Test/CPP/test.cpp
+++ Test/CPP/test.cpp
@@ -18,119 +18,194 @@ using namespace std;
 
 
 void test_LightSource(){
+    cout << "\n\nTesting \e[1mLightSource:\e[0m\n\n";
     LightSource LS = LightSource(10,5);
 
-    if (LS.getX()==10)
-        cout << "LightSource - getX - OK" << endl;
+    if (LS.getX()==10) cout << "\tLightSource -> getX() : works properly (same for Image)\n";
+    else cout << "\tLightSource -> getX() : works faulty (same for Image)\n";
+    if (LS.getY()==5) cout << "\tLightSource -> getY() : works properly (same for Image)\n";
+    else cout << "\tLightSource -> getY() : works faulty (same for Image)\n";
+    
+    LS.setX(15);
+    LS.setY(10);
+
+    if (LS.getX()==15) cout << "\tLightSource -> setX(double) : works properly (same for Image)\n";
+    else cout << "\tLightSource -> setX(double) : works faulty (same for Image)\n";
+    if (LS.getY()==10) cout << "\tLightSource -> setY(double) : works properly (same for Image)\n";
+    else cout << "\tLightSource -> setY(double) : works faulty (same for Image)\n";
+}
+
+void test_ThinLens(){
+    cout << "\n\nTesting \e[1mThinLens:\e[0m\n\n";
+    ThinLens ThinL = ThinLens(10,5);
+
+    if (ThinL.getX()==10) cout << "\tThinLens -> getX() : works properly\n";
+    else cout << "\tThinLens -> getX() : works faulty\n";
+    if (ThinL.getF()==5) cout << "\tThinLens -> getF() : works properly\n";
+    else cout << "\tThinLens -> getF() : works faulty\n";
+    
+    ThinL.setX(15);
+    ThinL.setF(10);
+
+    if (ThinL.getX()==15) cout << "\tThinLens -> setX(double) : works properly\n";
+    else cout << "\tThinLens -> setX(double) : works faulty\n";
+    if (ThinL.getF()==10) cout << "\tThinLens -> setF(double) : works properly\n";
+    else cout << "\tThinLens -> setF(double) : works faulty\n";
+    
 
 }
+
+void test_ThickLens(){
+    cout << "\n\nTesting \e[1mThickLens:\e[0m\n\n";
+    ThickLens ThickL = ThickLens(10, 1.5, 5, -20, 25);
+
+    if (ThickL.getX()==10) cout << "\tThickLens -> getX() : works properly\n";
+    else cout << "\tThickLens -> getX() : works faulty\n";
+    if (ThickL.getN()==1.5) cout << "\tThickLens -> getN() : works properly\n";
+    else cout << "\tThickLens -> getN() : works faulty\n";
+    if (ThickL.getD()==5) cout << "\tThickLens -> getD() : works properly\n";
+    else cout << "\tThickLens -> getD() : works faulty\n";
+    if (ThickL.getR_Left()==-20) cout << "\tThickLens -> getR_Left() : works properly\n";
+    else cout << "\tThickLens -> getR_Left() : works faulty\n";
+    if (ThickL.getR_Right()==25) cout << "\tThickLens -> getR_Right() : works properly\n";
+    else cout << "\tThickLens -> getR_Right() : works faulty\n";
+    
+    
+    ThickL.setX(20);
+    ThickL.setN(1.7);
+    ThickL.setD(3);
+    ThickL.setR_Left(15);
+    ThickL.setR_Right(10);
+
+    if (ThickL.getX()==20) cout << "\tThickLens -> setX(double) : works properly\n";
+    else cout << "\tThickLens -> setX(double) : works faulty\n";
+    if (ThickL.getN()==1.7) cout << "\tThickLens -> setN(double) : works properly\n";
+    else cout << "\tThickLens -> setN(double) : works faulty\n";
+    if (ThickL.getD()==3) cout << "\tThickLens -> setD(double) : works properly\n";
+    else cout << "\tThickLens -> setD(double) : works faulty\n";
+    if (ThickL.getR_Left()==15) cout << "\tThickLens -> setR_Left(double) : works properly\n";
+    else cout << "\tThickLens -> setR_Left(double) : works faulty\n";
+    if (ThickL.getR_Right()==10) cout << "\tThickLens -> setR_Right(double) : works properly\n";
+    else cout << "\tThickLens -> setR_Right(double) : works faulty\n";
+
+    ThickL.setN(1.5);
+    ThickL.setD(3);
+    ThickL.setR_Left(10);
+    ThickL.setR_Right(10);
+
+    if (ThickL.getF()==200) cout << "\tThickLens -> getF() : works properly\n";
+    else cout << "\tThickLens -> getF() : works faulty\n";
+}
+
+void test_OpticalSystem(){
+    cout << "\n\nTesting \e[1mOpticalSystem:\e[0m\n\n";
+    // Create OpticalSystem and OpticalObjects
+    OpticalSystem OS = OpticalSystem();
+    LightSource LS = LightSource(0,5);
+    ThinLens ThinL = ThinLens(10,5);
+    ThickLens ThickL = ThickLens(30, 1.5, 5, -20, 25);
+
+    // Add LightSource to the OpticalSystem & get LightSource from OpticalSystem & check identity
+    OS.add(LS);
+    LightSource LS_get = OS.getLightSource();
+    if (LS_get.getX() == LS.getX() && LS_get.getY() == LS.getY())
+        cout << "\tOpticalSystem -> add(LightSource) & getLightSource() : works properly\n";
+    else cout << "\tThinLens -> add(LightSource) || getLightSource() : works faulty\n";
+
+
+    // Add Lenses to the OpticalSystem & get Lenses from OpticalSystem & check identity
+    OS.add(ThinL, "Lens1");
+    OS.add(ThickL, "Lens2");
+    map<string, OpticalObject*> OOList = OS.getSystemElements();
+    ThinLens* ThinL_get = (ThinLens*) OOList["Lens1"];
+    ThickLens* ThickL_get = (ThickLens*) OOList["Lens2"];
+
+    if (ThinL_get->getX() == ThinL.getX() && ThinL_get->getF() == ThinL.getF() &&
+        ThickL_get->getX() == ThickL.getX() && ThickL_get->getN() == ThickL.getN() &&
+        ThickL_get->getD() == ThickL.getD() && ThickL_get->getR_Left() == ThickL.getR_Left() &&
+        ThickL_get->getR_Right() == ThickL.getR_Right())
+        cout << "\tOpticalSystem -> add(OpticalObject&, string) & getSystemElements() : works properly\n";
+    else cout << "\tThinLens -> add(OpticalObject&, string) || getSystemElements() : works faulty\n";
+
+    // Modify LightSource & get LightSource from OpticalSystem & check whether the modification was successful
+    OS.modifyLightSource("x",-20);
+    OS.modifyLightSource("y",10);
+    LS_get = OS.getLightSource();
+    if (LS_get.getX() == -20 && LS_get.getY() == 10)
+        cout << "\tOpticalSystem -> modifyLightSource(string, double) : works properly\n";
+    else cout << "\tThinLens -> modifyLightSource(string, double) : works faulty\n";
+
+    // Modify Lenses & get Lenses from OpticalSystem & check whether the modification was successful
+    OS.modifyOpticalObject("Lens1", "x", 0);
+    OS.modifyOpticalObject("Lens1", "f", 10);
+    OS.modifyOpticalObject("Lens2", "x", 30);
+    OS.modifyOpticalObject("Lens2", "n", 1.7);
+    OS.modifyOpticalObject("Lens2", "d", 3);
+    OS.modifyOpticalObject("Lens2", "r_left", 35);
+    OS.modifyOpticalObject("Lens2", "r_right", 40);
+
+    OOList = OS.getSystemElements();
+    ThinL_get = (ThinLens*) OOList["Lens1"];
+    ThickL_get = (ThickLens*) OOList["Lens2"];
+
+    if (ThinL_get->getX() == 0 && ThinL_get->getF() == 10 &&
+        ThickL_get->getX() == 30 && ThickL_get->getN() == 1.7 &&
+        ThickL_get->getD() == 3 && ThickL_get->getR_Left() == 35 &&
+        ThickL_get->getR_Right() == 40)
+        cout << "\tOpticalSystem -> modifyOpticalObject(string, string, double) : works properly\n";
+    else cout << "\tThinLens -> modifyOpticalObject(string, string, double) : works faulty\n";
+
+    // Remove Lens & get Lenses from OpticalSystem & check whether the remove was successful
+    OS.remove("Lens2");
+    OOList = OS.getSystemElements();
+    ThinL_get = (ThinLens*) OOList["Lens1"];
+
+    if (ThinL_get->getX() == 0 && ThinL_get->getF() == 10 &&
+        OOList.size() == 1) // check if we still have Lens1 & if we have only one lens
+        cout << "\tOpticalSystem -> remove(string) : works properly\n";
+    else cout << "\tThinLens -> remove(string) : works faulty\n";
+
+    // Calculate image formation & get image sequence & check for proper image formation
+    OS.Calculate();
+    vector<Image> IS =  OS.getImageSequence();
+    Image FinalI = IS.back();
+
+    if (FinalI.getX() == 20 && FinalI.getY() == -10 && FinalI.getReal() == 1)
+        cout << "\tOpticalSystem -> Calculate() &  getImageSequence(): works properly\n";
+    else cout << "\tThinLens -> Calculate() &  getImageSequence() : works faulty\n";
+
+    //
+    map<string, ray> Rays =  OS.getRays();
+    if (Rays["ray_1"].x[0] == -20 && Rays["ray_1"].x[1] == 0 && Rays["ray_1"].x[2] == 20 &&
+        Rays["ray_1"].y[0] == 10 && Rays["ray_1"].y[1] == 10 && Rays["ray_1"].y[2] == -10 &&
+        Rays["ray_2"].x[0] == -20 && Rays["ray_2"].x[1] == 0 && Rays["ray_2"].x[2] == 20 &&
+        Rays["ray_2"].y[0] == 10 && Rays["ray_2"].y[1] == 0 && Rays["ray_2"].y[2] == -10 )
+        cout << "\tOpticalSystem -> getRays(): works properly\n";
+    else cout << "\tThinLens -> getRays() : works faulty\n";
+
+    // Save the OpticalSystem into file & relaod it from file & check if we get the same OpticalSystem
+    OS.save("saved_system.json");
+    OpticalSystem OS2 = OpticalSystem("saved_system.json");
+
+    LS_get = OS.getLightSource();
+    OOList = OS.getSystemElements();
+    ThinL_get = (ThinLens*) OOList["Lens1"];
+
+    if (LS_get.getX() == -20 && LS_get.getY() == 10 &&
+        ThinL_get->getX() == 0 && ThinL_get->getF() == 10)
+        cout << "\tOpticalSystem -> save(string) & OpticalSystem(string) : works properly\n";
+    else cout << "\tThinLens -> save(string) & OpticalSystem(string) : works faulty\n";
+}
+
+
 
 int main(int argc, char* argv[]){
     try{
         test_LightSource();
-        /*
-        string input_file = "./input.json";
-        string output_file = "./output.txt";
-        bool should_I_print = false;
-        bool should_I_calc = true;
-        bool should_I_print_il = false;
-        bool should_I_print_rays = false;
-    	if (argc < 2) {
-    		throw OptiSimError("\033[1mDescription\033[0m: By default, this tool reads an "
-                        "optical system from a file called \"input.json\" and "
-                        "writes the calculation results to a file called \"output.txt\".\n"
-                        "\033[1mUsage\033[0m: OptiSim [OPTIONS]\n"
-                        "For more details run: OptiSim --help"
-                        );
-	    }
-
-	    for (int i = 1; i < argc; ++i){
-            command_path splitted = split(argv[i]);
-            if (string(argv[i]) == "-h" || string(argv[i]) == "--help"){
-                help();
-                if (argc == 2) should_I_calc = false;
-
-            } else if (string(argv[i]) == "-v" || string(argv[i]) == "--version"){
-                const char* version_string = getOptiSimVersionString();
-                cout << version_string << endl;
-                if (argc == 2) should_I_calc = false;
-
-            } else if (string(argv[i]) == "-p" || string(argv[i]) == "--print"){
-                should_I_print = true;
-
-            } else if (string(argv[i]) == "-r" || string(argv[i]) == "--rays"){
-                should_I_print_rays = true;
-
-            } else if (string(argv[i]) == "-il" || string(argv[i]) == "--imagelist"){
-                should_I_print_il = true;
-
-            } else if (splitted.command == "-i" || splitted.command == "--input"){
-                if (splitted.file == "") throw OptiSimError("Check help for correct usage:  OptiSim --help");
-                input_file = splitted.file;
-
-            } else if (splitted.command == "-o" || splitted.command == "--output"){
-                if (splitted.file == "") throw OptiSimError("Check help for correct usage:  OptiSim --help");
-                output_file = splitted.file;
-            }
-	    }
-        if (should_I_calc) {
-            // instantiate an Optical System object using a .json file
-            OpticalSystem my_system(input_file);
-            
-            // calculate the system's imaging and print the system into the console
-            Image final_image = my_system.Calculate();
-
-            // save the output information into a file
-            ofstream outputFile(output_file);
-            my_system.toString(outputFile);
-
-            // print the optical system parameters and result on the console
-            if (should_I_print) my_system.toString();
-
-            // expands the output file with the image list
-            if (should_I_print_il) {
-                vector<Image> imageSequence = my_system.getImageSequence();
-                vector<Image>::iterator it;
-                outputFile << "#\tImages"
-                << "\n-------------------------------------------------------------------------------\n";
-                outputFile << left
-                           << setw(15) << "X coordinate"
-                           << setw(15) << "Y coordinate"
-                           << "Is real?" << "\n\n";
-
-                for (it = imageSequence.begin(); it < imageSequence.end(); ++it) {
-                    outputFile << right << fixed << setprecision(6)
-                               << setw(12) << (*it).getX()
-                               << setw(15) << (*it).getY()
-                               << setw(10) << setprecision(1) << (*it).getReal()
-                               << "\n";
-                }
-                outputFile << "\n-------------------------------------------------------------------------------\n";
-            }
-            if (should_I_print_rays){
-                map<string, ray> rays = my_system.getRays();
-                outputFile << "#\tRays"
-                << "\n-------------------------------------------------------------------------------\n";
-                outputFile << right
-                           << setw(16) << "Ray 1"
-                           << setw(30) << "Ray 2"<< "\n\n";
-                outputFile << left
-                           << setw(15) << "X coordinate"
-                           << setw(15) << "Y coordinate"
-                           << setw(15) << "X coordinate"
-                           << setw(15) << "Y coordinate"<< "\n\n";
-                for (int i =0; i<rays["ray_1"].x.size(); i++){
-                    outputFile << right << fixed << setprecision(6)
-                               << setw(12) << rays["ray_1"].x[i]
-                               << setw(15) << rays["ray_1"].y[i]
-                               << setw(15) << rays["ray_2"].x[i]
-                               << setw(15) << rays["ray_2"].y[i]
-                               << "\n";
-                }
-                outputFile << "\n-------------------------------------------------------------------------------\n";
-            }
-        }
-    	*/
+        test_ThinLens();
+        test_ThickLens();
+        test_OpticalSystem();
+        
     }catch(exception& e) // Catch any standard exception or custom OptiSimError
     {
         cout << e.what() << "\n";
